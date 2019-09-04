83f74faf1b0e577b49c64d2765ec866d9bbed342
Nabilabilyas <nabila8297@gmail.com>
2019-09-04 11:52:37 +0700

    update Nabila

---
+++ resources/views/Penjual/index_penjual.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penjual</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/datatables.min.css">
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/datatables.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h1>Data Penjual</h1>
+		<hr>
+		<!-- Button trigger modal -->
+		<div>
+			<button type="button" class="btn btn-primary" id="buttonAdd">Tambah</button>
+			<br><br>
+			<span id="notif">
+				
+			</span>
+			<!-- <button type="button" class="btn btn-secondary" id="buttonEdit">Edit</button> -->
+		</div>
+	</div>
+</body>
+</html>
