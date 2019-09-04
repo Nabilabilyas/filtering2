b74da0658c7481356faaa973f2047c2d5948ff86
Nabilabilyas <nabila8297@gmail.com>
2019-09-04 16:13:06 +0700

    update Nabila

---
+++ resources/views/test_templating/dua.blade.php
@@ -0,0 +1,27 @@
+@extends('template.master')
+
+ @section('content')
+ <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <small>
+          
+        </small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Test</a></li>
+        <li class="active">cobaaja</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="box">
+          <div class="box-body">
+            <h4><b>Fitri Nabila Yasmine</b></h4>
+          </div>
+        </div>
+    </section>           
+    <!-- /.box -->
+      @endsection
