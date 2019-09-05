44dc32327ee78a9dc78737e8901556a92438acc0
Nabilabilyas <nabila8297@gmail.com>
2019-09-05 17:36:41 +0700

    17.36

--- app/Http/Controllers/HalamanUtamaController.php
+++ app/Http/Controllers/HalamanUtamaController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use App\Barang;
+use App\Lokasi;
+use App\Penjual;
+use App\Kategori;
 
 class HalamanUtamaController extends Controller
 {
     public function index()
     {
-        $nama_lokasi_par = GenderModel::select('gender')->get();
-        $nama_kategori_par = HobiModel::select('hobi')->get();
-        return view('HalamanUtama.index',['$nama_lokasi_par'=>$nama_lokasi_par],['$nama_kategori_par'=>$nama_kategori_par]); // ngelempar data array bentuknya json
+        $lokasis = Lokasi::select('kode_lokasi')->get();
+        $kategoris = Kategori::select('kode_kategori')->get();
+        $penjuals = Penjual::select('kode_penjual')->get();
+        $barangs = Barang::select('kode_barang','nama_barang')->get();
+      return view::make('home1.index_home')->with('lokasis',$lokasis)->with('kategoris',$kategoris)->with('penjuals',$penjuals)->with('barangs',$barangs); // ngelempar data array bentuknya json
     }}
